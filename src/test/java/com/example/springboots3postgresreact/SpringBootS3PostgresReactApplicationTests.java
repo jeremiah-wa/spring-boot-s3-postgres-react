f9803ada1f92410a10e5c7bc42b1858bfff5fa3b
Jeremiah Wangaruro <jeremiah.wangaruro@ucdconnect.ie>
2020-07-25 21:47:35 +0100

    init commit

---
+++ src/test/java/com/example/springboots3postgresreact/SpringBootS3PostgresReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboots3postgresreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootS3PostgresReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
